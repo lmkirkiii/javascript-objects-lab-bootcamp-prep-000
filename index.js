2f9bebad6808883a235092933d9860e31b8c0231
lmkirkiii@gmail.com Kirk <lmkirkiii@gmail.com>
2018-05-06 22:41:10 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -12,12 +12,12 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
   return object
 }
 function destructivelyDeleteFromObjectByKey(object, key){
-  var newObj = Object.assign({},object)
-  delete newObj.key;
-  return newObj
+  delete object.key;
+  return object
 }
 
 function deleteFromObjectByKey(object, key){
-  delete object.key;
-  return object
+  var newObj = Object.assign({},object)
+  delete newObj.key;
+  return newObj
 }
