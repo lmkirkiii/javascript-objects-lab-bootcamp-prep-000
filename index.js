fd12ebbe3dada5e567fe6cec10153bb36af63344
lmkirkiii@gmail.com Kirk <lmkirkiii@gmail.com>
2018-05-06 22:28:13 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,7 +5,5 @@ function updateObjectWithKeyAndValue(object, key, value){
   return object
 }
 function updateObjectWithKeyAndValue(object, key, value){
-  var object2 = object;
-  object2[key] = value;
-  return object2
+  return Object.assign({}, obj, {[key]: value}); 
 }
